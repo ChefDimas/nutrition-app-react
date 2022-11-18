f3132f6606591cd188e32b853d52b040f421556b
ChefDimas <dimalunik@aol.com>
2022-11-18 15:51:56 +0100

    Added styling to popup close icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import GeneralLayout from "./layouts/GeneralLayout";
-import Test from "./components/test";
 
 function App() {
   return (
     <GeneralLayout>
-      <Test />
     </GeneralLayout>
   );
 }
